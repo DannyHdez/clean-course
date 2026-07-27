ca58a93708c3be61684dc749b9a03679ec48e4b7
Daniel Hdez <65356881+DannyHdez@users.noreply.github.com>
2026-07-27 12:55:05 +0100

    refactor: apply SOLID principles
    Exercise 02 - Open and Closed Principle

--- src/solid/02-open-close-c.ts
+++ src/solid/02-open-close-c.ts
@@ -1,10 +1,18 @@
-import axios from 'axios';
+//import axios from 'axios';
 
 export class HttpClient{
 
+    // async get( url: string ){
+    //     const { data, status } = await axios.get(url);
+    //     return { data, status };
+    // }
+
     async get( url: string ){
-        const { data, status } = await axios.get(url);
-        return { data, status };
+
+        const answer = await fetch( url );
+        const data = await answer.json();
+
+        return { data, status: answer.status };
     }
     
 }
