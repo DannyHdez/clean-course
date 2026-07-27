80dfe9ad065f8bdea21dd1ec622cc0becb2f922e
Daniel Hdez <65356881+DannyHdez@users.noreply.github.com>
2026-07-27 14:11:08 +0100

    feat: Interface Segregation Principle
    Exercise 04

---
+++ src/solid/04-segregation.ts
@@ -0,0 +1,17 @@
+interface Bird{
+    fly(): void;
+    eat(): void;
+    run(): void;
+}
+
+class Tucan implements Bird{
+    public fly(){}
+    public eat(){}
+    public run(){}
+}
+
+class Humminbird implements Bird{
+    public fly(){}
+    public eat(){}
+    public run(){}
+}
